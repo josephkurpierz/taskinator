21f3062df26e6f09eaf13d5e4f911106edb108fd
Joseph Kurpierz <josephkurpierz@gmail.com>
2022-05-19 18:37:05 -0500

    added form reset

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,14 @@ var taskFormHandler = function (event) {
   event.preventDefault();
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
-
+  //check if inputs are empty
+  if (!taskNameInput || !taskTypeInput){
+    alert("You need to fill out the task form!");
+    return false;
+  };
+  
+  formEl.reset();
+  
   //package up data as an object
   var taskDataObj = {
     name: taskNameInput,
